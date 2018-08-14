be9a61b3de5639b0cd25ea3df711dde1c2536711
AngeloVitorio <42278661+AngeloVitorio@users.noreply.github.com>
2018-08-14 14:51:26 -0300

    Add files via upload

---
+++ Bis_atualizado/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BIS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css_adicional/estilo.css">
+    <!--COD PARA MATERIALIZE-->
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"/>
+<!-- LINK PACOTE ISLID-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="1e88e5 blue darken-1">
+<div class="nav-wraapper container">
+ <img src="img/icon1.png" alt="BIS" id="icu" class="brand-logo" >
+ <!--MENU NO MATERIALIZE-->
+ <ul id="nav-mobile" class="left hide-on-med-and-down">
+ <li><a href="index.html"> Home</a></li>
+ <li><a href="cftv.html"> CFTV</a></li>
+ <li><a href="Hardware.html"> Hardware</a></li>
+<li><a href="software.html">Software</a></li>
+<li><a href="sea.html">Solução em Automação</a></li>
+<li><a href="contato.php">Contato</a></li>
+<li><a href="#"> 85 3047-2807</a></li>
+</nav>
+</ul>
+</div>
+<!--COD PARA ISLID-->
+<div class="container">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+   </ol>
+          <div class="carousel-inner">
+             <div class="item active">
+            <img src="img/isl4.jpg" alt="Los Angeles" style="width:100%;">
+            <div class="carousel-caption">
+            <h3></h3>
+            <p></p>
+            </div>
+          </div>
+
+          <div class="item">
+            <img src="img/isl2.jpg" alt="Chicago" style="width:100%;">
+            <div class="carousel-caption">
+            <h3></h3>
+            <p></p>
+            </div>
+          </div>
+    
+          <div class="item">
+            <img src="emilio.jpg" alt="New York" style="width:100%;">
+            <div class="carousel-caption">
+            <h3></h3>
+            <p></p>
+            </div>
+          </div>
+  
+        </div> 
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only"></span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only"></span>
+            </a>
+            </div>
+         </div>
+
+<!--CORPO DA PÁGINA-->
+ <!--COD PARA MATERILIZE-->
+ <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+</body>
+</html>
